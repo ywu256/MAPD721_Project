6b159a119eeebaee13a19ff8c61952f965f43008
ywu256 <ywu256@my.centennialcollege.ca>
2025-04-04 22:04:05 -0400

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.yulingwu.mapd721_project"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.yulingwu.mapd721_project"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
