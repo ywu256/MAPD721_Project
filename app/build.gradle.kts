afc701389ea00e074680520a33a02c0ad5e791ca
ywu256 <ywu256@my.centennialcollege.ca>
2025-04-08 00:14:00 -0400

    Add dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,7 @@ android {
 
 dependencies {
 
+    implementation("androidx.datastore:datastore-preferences:1.0.0")
     implementation("androidx.navigation:navigation-compose:2.7.5")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
